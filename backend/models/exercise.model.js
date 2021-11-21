61bdb418bedd5731c6e10e8577af5c6513bf7dba
jltimmer <jltimmer@users.noreply.github.com>
2021-11-21 03:55:34 -0500

    Finish backend - users and exercises

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -11,4 +11,6 @@ const exerciseSchema = new Schema({
   timestamps: true,
 });
 
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
 module.exports = Exercise;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +18,9 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
