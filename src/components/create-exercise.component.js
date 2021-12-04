eefc16c6caa7c1871d3dbf2e834e723f07ac27d8
jltimmer <jltimmer@users.noreply.github.com>
2021-12-04 08:12:30 -0500

    Add some frontend pieces

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(e) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+
+    console.log(exercise);
+    window.location = '/';
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Exercise component!</p>
+      </div>
+    )
+  }
+}
