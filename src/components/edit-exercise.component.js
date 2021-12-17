caa5ceedea8bf8462deb40c66e2da1aa1c2c0f36
jltimmer <jltimmer@users.noreply.github.com>
2021-12-16 19:58:00 -0500

    Downgrade react-router-dom to fix class-level routing

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -2,7 +2,6 @@ import { React, Component } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 export default class EditExercise extends Component {
